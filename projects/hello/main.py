3dc4ba4cb94fe3ca13b0c1df0b0e69d04778805c
Nikolay Zakharevich <nikolayzakharevich@gmail.com>
2022-08-22 09:22:21 +0200

    7

--- projects/hello/main.py
+++ projects/hello/main.py
@@ -1,8 +1,7 @@
-from lib1 import lib
-from python_hello import hello_world
+from lib1_main import lib
 
 def main():
-    print("Hello, world" + lib.get_lib_name())
+    print("Hello, world [" + lib.get_lib_name() + "]")
 
 
 if __name__ == '__main__':
