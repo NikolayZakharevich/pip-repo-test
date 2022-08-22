f132b0fdaaf2de948c9d74e5ae8ea557633e6bcb
Nikolay Zakharevich <nikolayzakharevich@gmail.com>
2022-08-22 08:53:24 +0200

    4

---
+++ packages/lib1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="lib1",
+    version="0.0.1",
+    author="Nikolay Zakharevich",
+    author_email="nikolayza@gmail.com",
+    description="A function that returns 'lib1'",
+    url="https://github.com/ceddlyburge/python_world",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
