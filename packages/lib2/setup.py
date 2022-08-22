89d1a5cdec3187d3d7a51ef653f5d0645c6a8520
Nikolay Zakharevich <nikolayzakharevich@gmail.com>
2022-08-22 09:03:34 +0200

    4

--- packages/lib2/setup.py
+++ packages/lib2/setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
 setuptools.setup(
-    name="lib1",
+    name="lib2",
     version="0.0.1",
     author="Nikolay Zakharevich",
     author_email="nikolayzakharevich@gmail.com",
-    description="A function that returns 'lib1'",
+    description="A function that returns 'lib2'",
     url="https://github.com/NikolayZakharevich/pip-repo-test",
     packages=setuptools.find_packages(),
     classifiers=[
--- projects/hello/main.py
+++ projects/hello/main.py
@@ -1,5 +1,8 @@
+from lib1 import lib
+from python_hello import hello_world
+
 def main():
-    print("Hello, world")
+    print("Hello, world" + lib.get_lib_name())
 
 
 if __name__ == '__main__':
